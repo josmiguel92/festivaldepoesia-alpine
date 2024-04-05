8f2977e7f690ecc0a0f47a459f5c54533af71676
Josué Miguel Aguilar Polo <josmiguel92@gmail.com>
2024-04-05 12:38:51 +0200

    update logo size, add article

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -24,8 +24,8 @@ const show = ref(false)
       <NuxtLink v-if="alpine.header.logo" to="/">
         <!-- TODO: width/height doesn't make much sense here, but Lighthouse requires it. 
         Used current Alpine logo ratio, will break on enduser logo -->
-        <NuxtImg class="dark-img" :src="alpine.header.logo.pathDark" :alt="alpine.header.logo.alt" width="89" height="31" />
-        <NuxtImg class="light-img" :src="alpine.header.logo.path" :alt="alpine.header.logo.alt" width="89" height="31" />
+        <NuxtImg class="dark-img" :src="alpine.header.logo.pathDark" :alt="alpine.header.logo.alt" width="89" height="31" style="width: 10rem;height: 4rem;"/>
+        <NuxtImg class="light-img" :src="alpine.header.logo.path" :alt="alpine.header.logo.alt" width="89" height="31" style="width: 10rem;height: 4rem;"/>
       </NuxtLink>
       <NuxtLink v-else to="/" class="fallback">
         {{ alpine.title }}
